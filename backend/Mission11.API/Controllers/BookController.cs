bf24d1fa427e07f05af5459b88b4b5b9edbc579d
AutPort <ajs289@byu.edu>
2025-03-28 16:10:17 -0600

    git ignores added

--- backend/Mission11.API/Controllers/BookController.cs
+++ backend/Mission11.API/Controllers/BookController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Mission11.API.Data;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Mission11.API.Controllers
 {
@@ -13,14 +14,21 @@ namespace Mission11.API.Controllers
         public BookController(BookDbContext temp) => _bookContext = temp;
 
         [HttpGet("AllBooks")]
-        public IActionResult GetBooks(int pageSize = 10, int pageNum = 1)
+        public IActionResult GetBooks(int pageSize = 10, int pageNum = 1, [FromQuery] List<string>? bookCategories = null)
         {
-            var something = _bookContext.Books
+            var query = _bookContext.Books.AsQueryable();
+
+            if (bookCategories != null && bookCategories.Any()) // change from project types to book categories!!
+            {
+                query = query.Where(b => bookCategories.Contains(b.Category));
+            }
+
+            var totalNumBooks = query.Count();
+
+            var something = query
                 .Skip((pageNum - 1) * pageSize)
                 .Take(pageSize)
                 .ToList();
-
-            var totalNumBooks = _bookContext.Books.Count();
 
             var someObject = new
             {
@@ -31,6 +39,16 @@ namespace Mission11.API.Controllers
             return Ok(someObject);
         }
 
+        // inlcude httpget for book categories
+        [HttpGet("GetBookCategories")]
+        public IActionResult GetBookCategories ()
+        {
+            var bookCategories = _bookContext.Books
+                .Select(b => b.Category)
+                .Distinct()
+                .ToList();
 
+            return Ok(bookCategories);
+        }
     }
 }
